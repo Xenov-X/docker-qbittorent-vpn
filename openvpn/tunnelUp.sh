a3c2838290690c40b518a0952628423a0a401edb
Xenov <62152036+Xenov-X@users.noreply.github.com>
2020-12-19 18:01:38 +0000

    Update tunnelUp.sh

--- openvpn/tunnelUp.sh
+++ openvpn/tunnelUp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 /usr/share/openrc/support/openvpn/up.sh
 
-/etc/transmission/start.sh "$@"
+#/etc/transmission/start.sh "$@"
 
 /qbittorrent/qbittorent-lockinterface-interface.sh "$@"
 /qbittorrent/qbittorent-lockinterface-IP.sh "$@"
@@ -13,6 +13,7 @@ done
 # $1 is tun0 
 # $3 is VPN IP interface 
 /etc/openvpn/DNS-Leak-Test.sh
-
+echo "starting qbitorrent"
 qbittorrent-nox -d --webui-port=8080 --profile=/config --save-path=/downloads
+echo "qbittorrent laucnched on localhost:8080"
 [[ ! -f /opt/tinyproxy/start.sh ]] || /opt/tinyproxy/start.sh
