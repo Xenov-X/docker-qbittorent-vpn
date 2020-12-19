7cb73d0b8e1ba0e13988e035b27132626541480d
Xenov <62152036+Xenov-X@users.noreply.github.com>
2020-12-19 17:46:25 +0000

    Update healthcheck.sh

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -24,16 +24,16 @@ echo "Network is up"
 #Service check
 #Expected output is 2 for both checks, 1 for process and 1 for grep
 OPENVPN=$(pgrep openvpn | wc -l )
-TRANSMISSION=$(pgrep transmission | wc -l)
+qbittorrent=$(pgrep qbittorrent-nox | wc -l)
 
 if [[ ${OPENVPN} -ne 1 ]]; then
 	echo "Openvpn process not running"
 	exit 1
 fi
-if [[ ${TRANSMISSION} -ne 1 ]]; then
-	echo "transmission-daemon process not running"
+if [[ ${qbittorrent} -ne 1 ]]; then
+	echo "qbittorrent-daemon process not running"
 	exit 1
 fi
 
-echo "Openvpn and transmission-daemon processes are running"
+echo "Openvpn and qbittorrent-daemon processes are running"
 exit 0
