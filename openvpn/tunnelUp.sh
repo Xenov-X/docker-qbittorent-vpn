4ab868d484518644be65f43a29a5adc79ccc76d0
Xenov <62152036+Xenov-X@users.noreply.github.com>
2020-12-19 15:58:47 +0000

    Update tunnelUp.sh

--- openvpn/tunnelUp.sh
+++ openvpn/tunnelUp.sh
@@ -2,5 +2,10 @@
 /usr/share/openrc/support/openvpn/up.sh
 /etc/openvpn/DNS-Leak-Test.sh
 /etc/transmission/start.sh "$@"
+
+until ifconfig | grep tun | grep -q "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00"; do
+    sleep 0.5
+done
+
 qbittorrent-nox
 [[ ! -f /opt/tinyproxy/start.sh ]] || /opt/tinyproxy/start.sh
